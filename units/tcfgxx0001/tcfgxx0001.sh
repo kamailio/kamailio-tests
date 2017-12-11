eeee3400a5aad2681d6d569795abec840df4e156
Daniel-Constantin Mierla <miconda@gmail.com>
2017-12-11 12:03:52 +0100

    tcfgxx0001: added variants to start default config with all -A options

--- units/tcfgxx0001/tcfgxx0001.sh
+++ units/tcfgxx0001/tcfgxx0001.sh
@@ -10,6 +10,7 @@ if [ ! "$ret" -eq 0 ] ; then
     exit $ret
 fi
 
+echo
 echo "--- start with default config"
 ${KAMBIN} -P ${KAMPID} -w ${KAMRUN} -Y ${KAMRUN} -a no
 ret=$?
@@ -19,8 +20,45 @@ if [ ! "$ret" -eq 0 ] ; then
     exit $ret
 fi
 
-echo "--- start with default config and several -A options (auth, ipauth, usrlodb ..."
-${KAMBIN} -P ${KAMPID} -w ${KAMRUN} -Y ${KAMRUN} -a no -A WITH_DEBUG -A WITH_MYSQL -A WITH_IPAUTH -A WITH_USRLOCDB
+echo
+echo "--- start with default config and -A WITH_DEBUG"
+${KAMBIN} -P ${KAMPID} -w ${KAMRUN} -Y ${KAMRUN} -a no -A WITH_DEBUG
+ret=$?
+sleep 1
+kill_pidfile ${KAMPID}
+if [ ! "$ret" -eq 0 ] ; then
+    exit $ret
+fi
+
+echo
+echo "--- start with default config and main -A options (auth, ipauth, usrlodb)"
+${KAMBIN} -P ${KAMPID} -w ${KAMRUN} -Y ${KAMRUN} -a no -E -e -A WITH_MYSQL -A WITH_IPAUTH -A WITH_USRLOCDB
+ret=$?
+sleep 1
+kill_pidfile ${KAMPID}
+if [ ! "$ret" -eq 0 ] ; then
+    exit $ret
+fi
+
+echo
+echo "--- start with default config and all non-debug -A options (auth, ipauth, usrlodb, nat, presence ..."
+${KAMBIN} -P ${KAMPID} -w ${KAMRUN} -Y ${KAMRUN} -a no -E -e -A WITH_MYSQL \
+         -A WITH_AUTH \
+         -A WITH_IPAUTH \
+         -A WITH_USRLOCDB \
+         -A WITH_PRESENCE \
+         -A WITH_NAT \
+         -A WITH_PSTN \
+         -A WITH_ALIASDB \
+         -A WITH_SPEEDDIAL \
+         -A WITH_MULTIDOMAIN \
+         -A WITH_TLS \
+         -A WITH_XMLRPC \
+         -A WITH_ANTIFLOOD \
+         -A WITH_BLOCK3XX \
+         -A WITH_BLOCK401407 \
+         -A WITH_VOICEMAIL \
+         -A WITH_ACCDB
 ret=$?
 sleep 1
 kill_pidfile ${KAMPID}
