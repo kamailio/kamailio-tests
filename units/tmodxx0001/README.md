90799c01e28446e0accacf61e49b8644ec9bfea9
Daniel-Constantin Mierla <miconda@gmail.com>
2017-12-11 11:37:18 +0100

    tmodxx0001: run variants to cover modules that have load conflicts

--- units/tmodxx0001/README.md
+++ units/tmodxx0001/README.md
@@ -2,8 +2,11 @@
 
 Summary: load all modules - detect undefined symbols
 
-All modules that compile on the guest OS and don't have init constraints are loaded, the rest are commented, see `kamailio-allmods.cfg`.
+All modules that compile on the guest OS and don't have init constraints are loaded,
+the rest are commented, see `kamailio-allmods.cfg`. Some modules have load conflicts
+because they define same variables (e.g., $rtpstat is defined by rtpengine and rtpproxy).
 
 Following tests are done:
 
-  * run kamailio -f kamailio-allmods.cfg
+  * run `kamailio -f kamailio-allmods.cfg`
+  * run `kamailio -f kamailio-allmods.cfg -A WITH_IMS -A WITH_RTPPROXY`
--- units/tmodxx0001/tmodxx0001.sh
+++ units/tmodxx0001/tmodxx0001.sh
@@ -7,9 +7,20 @@ echo "--- start kamailio -f ./kamailio-allmods.cfg"
 ${KAMBIN} -f ./kamailio-allmods.cfg -dd -E 2>&1 | tee /tmp/kamailio-allmods.log
 echo
 echo "--- grep output"
+echo
 grep "undefined symbol" /tmp/kamailio-allmods.log
 ret=$?
-if [ "$ret" -eq 1 ] ; then
-    exit 0
+if [ ! "$ret" -eq 1 ] ; then
+    exit 1
 fi
-exit 1
+echo "--- start kamailio -f ./kamailio-allmods.cfg -A WITH_IMS -A WITH_RTPPROXY"
+${KAMBIN} -f ./kamailio-allmods.cfg -dd -E  -A WITH_IMS -A WITH_RTPPROXY 2>&1 | tee /tmp/kamailio-allmods.log
+echo
+echo "--- grep output"
+echo
+grep "undefined symbol" /tmp/kamailio-allmods.log
+ret=$?
+if [ ! "$ret" -eq 1 ] ; then
+    exit 1
+fi
+exit 0
