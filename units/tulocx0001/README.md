1492740408819469c3610656c833f72820be41aa
Daniel-Constantin Mierla <miconda@gmail.com>
2017-12-23 18:09:47 +0100

    tulocx0001: added unit for basic usrloc tests

---
+++ units/tulocx0001/README.md
@@ -0,0 +1,8 @@
+# User Location - Basic Tests #
+
+Summary: user location - basic tests
+
+Following tests are done:
+
+  * run kamailio with default config file, do a registration and a signaling
+  only call using sipsak
---
+++ units/tulocx0001/tulocx0001.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+. ../../etc/config
+. ../../libs/utils
+
+echo
+echo "--- start with default config"
+${KAMBIN} -P ${KAMPID} -w ${KAMRUN} -Y ${KAMRUN} -a no
+ret=$?
+sleep 1
+if [ ! "$ret" -eq 0 ] ; then
+    exit $ret
+fi
+echo
+echo "--- start sipsak -vvv -U -I -H 127.0.0.1 -s sip:alice@127.0.0.1"
+sipsak -vvv -U -I -H 127.0.0.1 -s sip:alice@127.0.0.1 2>&1 | tee /tmp/kamailio-tulocx0001.log &
+sleep 1
+echo
+echo "--- grep output"
+echo
+grep "usrloc for alice completed successful" /tmp/kamailio-tulocx0001.log
+ret=$?
+if [ ! "$ret" -eq 0 ] ; then
+    exit 1
+fi
+kill_pidfile ${KAMPID}
+
+exit $ret
