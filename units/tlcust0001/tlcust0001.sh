259d0e49ec51c20dba5975a9638523780f299d14
Daniel-Constantin Mierla <miconda@gmail.com>
2018-09-17 15:19:19 +0200

    units/tlcust0001: info about executed steps

--- units/tlcust0001/tlcust0001.sh
+++ units/tlcust0001/tlcust0001.sh
@@ -14,14 +14,14 @@ echo "--- restart nc -ulp 24680 | tee /tmp/kamailio-tlcust0001.netcat &"
 killall nc
 nc -ulp 24680 | tee /tmp/kamailio-tlcust0001.netcat &
 echo "--- run sipsak -s sip:alice@127.0.0.1"
-sipsak -vvv -s sip:alice@127.0.0.1
+sipsak -s sip:alice@127.0.0.1
 sleep 1
 kill_pidfile ${KAMPID}
 sleep 1
 killall nc
 sleep 1
 echo
-echo "--- grep output"
+echo "--- output files"
 echo
 ls -l /tmp
 echo "---"
@@ -29,6 +29,7 @@ cat /tmp/kamailio-tlcust0001.netcat
 echo "---"
 cat /tmp/kamailio-tlcust0001.log
 echo "---"
+echo "--- grep output"
 grep "logcustom" /tmp/kamailio-tlcust0001.netcat
 ret=$?
 if [ ! "$ret" -eq 0 ] ; then
