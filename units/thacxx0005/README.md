b434189e1eb800cb32b986e8bcdeb690df43df0d
Giacomo Vacca <giacomo.vacca@gmail.com>
2021-04-30 19:01:48 +0200

    thacxx0005 - http_async_client suspend on reply

---
+++ units/thacxx0005/README.md
@@ -0,0 +1,8 @@
+# http_aysnc_client - suspend replies #
+
+Summary: http_async_client - suspend replies
+
+Following tests are done:
+
+  * run kamailio with kamailio-thacxx0005.cfg and test suspending a transaction on a reply
+
---
+++ units/thacxx0005/thacxx0005.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+. ../../etc/config
+. ../../libs/utils
+
+python3 http_server.py &
+
+echo "--- start kamailio -f ./kamailio-thacxx0005.cfg"
+${KAMBIN} -P ${KAMPID} -w ${KAMRUN} -Y ${KAMRUN} -f ./kamailio-thacxx0005.cfg -a no -ddd -E 2>&1 | tee /tmp/kamailio-thacxx0005.log &
+#${KAMBIN} -P ${KAMPID} -w ${KAMRUN} -Y ${KAMRUN} -f ./kamailio-thacxx0005.cfg -a no -dd -E 2>&1 | tee /tmp/kamailio-thacxx0005.log &
+ret=$?
+
+sleep 5
+
+echo "-------------- starting sipp UAS -------------------------"
+sipp -sf uas.xml -p 5080 -bg
+
+
+sleep 1
+
+echo "-------------- starting sipp UAC -------------------------"
+sipp -sf uac.xml -m 1 127.0.0.1:5060
+
+tail /tmp/kamailio-thacxx0005.log
+
+echo
+grep "HTTP_REPLY2" /tmp/kamailio-thacxx0001.log
+ret=$?
+if [ ! "$ret" -eq 0 ] ; then
+    exit 1
+fi
+exit 0
+
