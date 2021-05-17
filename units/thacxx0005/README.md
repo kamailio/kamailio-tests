0279010e6b294f007977e52e215503e8499c99b5
Giacomo Vacca <giacomo.vacca@gmail.com>
2021-05-17 12:29:53 +0200

    thacxx0005 - http_async_client suspend on reply (#19)
    * thacxx0006 - re-suspend a request to perform an HTTP query

---
+++ units/thacxx0005/README.md
@@ -0,0 +1,8 @@
+# http_aysnc_client - Re-suspend reply #
+
+Summary: http_async_client - Re-suspend reply
+
+Following tests are done:
+
+  * run kamailio with kamailio-thacxx0005.cfg and test if re-suspending a reply to perform another HTTP query works
+
---
+++ units/thacxx0005/thacxx0005.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+. ../../etc/config
+. ../../libs/utils
+
+python3 http_server.py &
+
+echo "--- start kamailio -f ./kamailio-thacxx0005.cfg"
+${KAMBIN} -P ${KAMPID} -w ${KAMRUN} -Y ${KAMRUN} -f ./kamailio-thacxx0005.cfg -a no -dd -E 2>&1 | tee /tmp/kamailio-thacxx0005.log &
+ret=$?
+
+sleep 3
+
+echo "-------------- starting sipp UAS -------------------------"
+sipp -sf uas.xml -p 5080 -bg
+
+sleep 1
+
+NCALLS=20
+echo "-------------- starting sipp UAC -------------------------"
+sipp -sf uac.xml -m ${NCALLS} 127.0.0.1:5060
+
+kill_pidfile ${KAMPID}
+sleep 1
+killall python3
+sleep 1
+killall sipp
+
+echo
+count="$(grep -c "HTTP_REPLY2" /tmp/kamailio-thacxx0005.log)"
+if [ ! $count -eq $NCALLS ] ; then
+    exit 1
+fi
+exit 0
