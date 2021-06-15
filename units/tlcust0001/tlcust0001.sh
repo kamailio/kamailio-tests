59618d1facd785e9961575f0a21fcf120760b09e
Daniel-Constantin Mierla <miconda@gmail.com>
2021-06-15 18:04:22 +0200

    units: tlcust0001 - updates with explicit listen on loopback

--- units/tlcust0001/tlcust0001.sh
+++ units/tlcust0001/tlcust0001.sh
@@ -4,14 +4,16 @@
 . ../../libs/utils
 
 
-echo "--- start nc -ulp 24680 | tee /tmp/kamailio-tlcust0001.netcat &"
-nc -ulp 24680 | tee /tmp/kamailio-tlcust0001.netcat &
+echo "--- start nc -u -l -p 24680 | tee /tmp/kamailio-tlcust0001.netcat &"
+rm -f /tmp/kamailio-tlcust0001.netcat
+nc -u -l -p 24680 | tee /tmp/kamailio-tlcust0001.netcat &
+sleep 1
 echo "--- start kamailio -f ./kamailio-tlcust0001.cfg"
 ${KAMBIN} -P ${KAMPID} -w ${KAMRUN} -Y ${KAMRUN} -f ./kamailio-tlcust0001.cfg -a no 2>&1 | tee /tmp/kamailio-tlcust0001.log &
 ret=$?
-sleep 1
+sleep 2
 echo "--- run sipsak -s sip:alice@127.0.0.1"
-sipsak -s sip:alice@127.0.0.1
+sipsak -vvv -s sip:alice@127.0.0.1:5060
 sleep 1
 kill_pidfile ${KAMPID}
 sleep 1
@@ -21,9 +23,9 @@ echo
 echo "--- output files"
 echo
 ls -l /tmp
-echo "---"
+echo "--- /tmp/kamailio-tlcust0001.netcat"
 cat /tmp/kamailio-tlcust0001.netcat
-echo "---"
+echo "--- /tmp/kamailio-tlcust0001.log"
 cat /tmp/kamailio-tlcust0001.log
 echo "---"
 echo "--- grep output"
