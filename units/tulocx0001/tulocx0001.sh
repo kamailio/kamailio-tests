63bf81698cc1eb201fa4927c70916f42d10af117
Daniel-Constantin Mierla <miconda@gmail.com>
2023-05-09 12:05:47 +0200

    tulocx0001: updated for sipsak 0.9.8

--- units/tulocx0001/tulocx0001.sh
+++ units/tulocx0001/tulocx0001.sh
@@ -4,7 +4,7 @@
 . ../../libs/utils
 
 echo
-echo "--- start with default config"
+echo "--- start with default config: ${KAMBIN} -P ${KAMPID} -w ${KAMRUN} -Y ${KAMRUN} -a no"
 ${KAMBIN} -P ${KAMPID} -w ${KAMRUN} -Y ${KAMRUN} -a no
 ret=$?
 sleep 1
@@ -12,13 +12,13 @@ if [ ! "$ret" -eq 0 ] ; then
     exit $ret
 fi
 echo
-echo "--- start sipsak -vvv -U -I -H 127.0.0.1 -s sip:alice@127.0.0.1"
-sipsak -vvv -U -I -H 127.0.0.1 -s sip:alice@127.0.0.1 2>&1 | tee /tmp/kamailio-tulocx0001.log &
+echo "--- start sipsak -vvv -U -i -l 15098 -H 127.0.0.1 -s sip:alice@127.0.0.1"
+sipsak -vvv -U -i -l 15098 -H 127.0.0.1 -s sip:alice@127.0.0.1:5060 2>&1 | tee /tmp/kamailio-tulocx0001.log &
 sleep 1
 echo
 echo "--- grep output"
 echo
-grep "usrloc for alice completed successful" /tmp/kamailio-tulocx0001.log
+grep "SIP/2.0 200 OK" /tmp/kamailio-tulocx0001.log
 ret=$?
 if [ ! "$ret" -eq 0 ] ; then
     exit 1
