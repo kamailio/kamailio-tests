93daa30c71dd4afd38b8a36f49da2c3e5a6dbe0c
Daniel-Constantin Mierla <miconda@gmail.com>
2026-09-23 10:18:11 +0200

    tcfgxx0002: use sipexer to send sip traffic

--- units/tcfgxx0002/README.md
+++ units/tcfgxx0002/README.md
@@ -4,4 +4,4 @@ Summary: default config file - basic sip signaling tests
 
 Following tests are done:
 
-  * load with default config file and sent OPTIONS with sipsak to sip:127.0.0.1
+  * load with default config file and sent OPTIONS with sipexer to sip:127.0.0.1
--- units/tcfgxx0002/tcfgxx0002.sh
+++ units/tcfgxx0002/tcfgxx0002.sh
@@ -3,14 +3,18 @@
 . ../../etc/config
 . ../../libs/utils
 
-${KAMBIN} -P ${KAMPID} -w ${KAMRUN} -Y ${KAMRUN} -a no
+${KAMBIN} -P ${KAMPID} -w ${KAMRUN} -Y ${KAMRUN} ${KAMPRM} -a no
 ret=$?
 sleep 1
 if [ ! "$ret" -eq 0 ] ; then
     exit $ret
 fi
-sipsak -v -s sip:127.0.0.1
+# sipexer returns the last sip status code
+sipexer -sd -to-uri sip:127.0.0.1 sip:127.0.0.1
 ret=$?
 kill_pidfile ${KAMPID}
 
-exit $ret
+if [ ! "$ret" -eq 200 ] ; then
+    exit 1
+fi
+exit 0
